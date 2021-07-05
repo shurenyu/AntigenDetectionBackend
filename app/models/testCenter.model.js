c1122d8cf3328f717d969e622344f12e9c0339a6
dev_shuren <chepali123@hotmail.com>
2021-07-05 21:43:10 +0800

    first commit

---
+++ app/models/testCenter.model.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, Sequelize) => {
+    return sequelize.define("testCenter", {
+        name: {
+            type: Sequelize.STRING(100)
+        },
+        address: {
+            type: Sequelize.STRING(100)
+        },
+        latitude: {
+            type: Sequelize.DOUBLE
+        },
+        longitude: {
+            type: Sequelize.DOUBLE
+        },
+        stamp: {
+            type: Sequelize.STRING(255)
+        },
+        active: {
+            type: Sequelize.INTEGER(4),
+            defaultValue: 1,
+        },
+        updatedDate: {
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.NOW
+        },
+        createdDate: {
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.NOW
+        }
+    }, {
+        timestamps: false,
+        underscored: false,
+    });
+};
